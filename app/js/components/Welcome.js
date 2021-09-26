79eedbd4eee07e3be5aa81d8b6071d7b320b8721
valcaro87@gmail.com <valcaro87@gmail.com>
2021-09-26 10:15:36 +0800

    react-component-validators

--- app/js/components/Welcome.js
+++ app/js/components/Welcome.js
@@ -4,9 +4,39 @@ export default class Welcome extends React.Component {
     render() {
         return (
             <div>
-                <h3> Welcome  {this.props.fname} {this.props.lname} Number: {this.props.number}</h3>
+                <h3> {this.props.fname} {this.props.lname} Number: {this.props.number}</h3>
+                <h4>{this.props.elementz}</h4>
             </div>
         )
 
     }
 }
+
+const lengthChecker = ((props, propName, component) => {
+    if (props[propName]) {
+        let val = props[propName]
+        if (typeof val === 'string') {
+            return val.length >= 5 ? null : new Error(`${propName} in ${component} is too short`); //new Error(propName + component + 'is too short')
+        }
+    }
+    return null;
+})
+
+Welcome.propTypes = {
+    fname: lengthChecker,
+    lname: React.PropTypes.string,
+    number: React.PropTypes.string,
+    elementz: React.PropTypes.oneOfType([
+        React.PropTypes.string,
+        React.PropTypes.number,
+        React.PropTypes.element,
+    ])
+}
+
+Welcome.defaultProps = {
+    number: "i dont have number"
+}
+
+// React.PropTypes.number, //element
+// React.PropTypes.string, //bool //element //object
+// string.isRequired, //element //number
