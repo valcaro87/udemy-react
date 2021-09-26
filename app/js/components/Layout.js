c69afe2e850dbffb5b78935b2eed94a8083f988d
valcaro87@gmail.com <valcaro87@gmail.com>
2021-09-26 11:21:49 +0800

    react-state-concepts-extract-separate-component

---
+++ app/js/components/Layout.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from './Button';
+
+
+export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            fname: 'chris',
+            lname: 'caro'
+        }
+    }
+
+    changeName() {
+        console.log(this);
+        this.setState({
+            fname: 'val'
+        })
+    }
+
+    render() {
+        return (
+            <div >
+                <h1> {this.state.fname} </h1>
+                <p>
+                    lorem ipsum
+                </p>
+                <Button changeName={this.changeName.bind(this)} fname={this.state.fname} />
+            </div >
+        )
+
+
+    }
+
+}
