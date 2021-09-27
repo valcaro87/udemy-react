29647bcf35c8825687fdae60fc385ff091c4eea8
valcaro87@gmail.com <valcaro87@gmail.com>
2021-09-27 10:45:07 +0800

    creating a timer

---
+++ app/js/components/Timer/TimerButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class TimerButton extends React.Component {
+    constructor() {
+        super()
+
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.props.handleClick}>
+                    {this.props.isStarted ? 'Stop' : 'Start'}
+                </button>
+            </div>
+        )
+    }
+
+}
---
+++ app/js/components/Timer/TimerHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const TimerHeader = ((props) =>
+    <h3>
+        i've been runnning for: {props.time} seconds - <span>{props.time > 5 ? 'timer is above 5' : 'timer is below 5'}</span>
+    </h3>
+)
