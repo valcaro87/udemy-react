9d494dab707dea0f984092a8a0f4cf191cd40139
valcaro87@gmail.com <valcaro87@gmail.com>
2021-10-09 01:24:41 +0800

    css modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,35 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
 	entry: "./app/js/app.js",
 	output: {
 		path: __dirname + '/public',
-		filename: 'bundle.js'
+		filename: 'bundle.js',
 	},
 	module: {
 		loaders: [
 			{
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
-				loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015'],
-			}
+				loaders: [
+					'react-hot', 
+					'babel?presets[]=react,presets[]=es2015'
+				],
+			}, 
+			{
+				test: /\.css$/,
+				loaders: [
+					'style-loader', 
+					'css-loader?modules=true,localIdentName=[name]__[local]__[hash:base64:5],camelCase=true'
+				]
+			},
 		]
 	},
 	devServer: {
 		historyApiFallback: true
-	}
+	},
+	plugins: [
+		new ExtractTextPlugin('styles.css'),
+	]
 }
+
